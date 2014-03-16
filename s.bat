0659984871c2c5989e9f8de6cad6e19f89aa8d02
root <root@licface.net>
2014-03-16 11:57:25 +0700

    update gitmaster.py

--- s.bat
+++ s.bat
@@ -5,20 +5,20 @@ if %1==-t goto total
 if %1==-s goto size
 if %1==-d goto detail
 
-c:\cygwin\bin\du -s -h %1
+d:\cygwin\bin\du -s -h %1
 goto end
 
 :size
-c:\cygwin\bin\du -h --max-depth=1 %2
+d:\cygwin\bin\du -h --max-depth=1 %2
 goto end
 
 :detail
-c:\cygwin\bin\du -h -a --max-depth=1 %2
+d:\cygwin\bin\du -h -a --max-depth=1 %2
 goto end
 
 
 :total
-c:\cygwin\bin\du -s -h %2
+d:\cygwin\bin\du -s -h %2
 goto end
 
 :usage
@@ -33,7 +33,7 @@ echo.
 echo.
 echo  please wait ..............
 echo.
-c:\cygwin\bin\du -s -h %CD%
+d:\cygwin\bin\du -s -h %CD%
 goto end
 
 :end
